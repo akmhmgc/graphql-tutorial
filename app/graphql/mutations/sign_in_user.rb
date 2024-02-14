d725fd337752c9eb39f9797ff8838a44c49b8290
akmhmgc <akimuhamaguchi@icloud.com>
2024-02-14 22:23:52 +0900

    Add token to session cookie

--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -22,6 +22,9 @@ module Mutations
       crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
       token = crypt.encrypt_and_sign("user-id:#{ user.id }")
 
+      # cookieに保存する
+      context[:session][:token] = token
+
       { user: user, token: token }
     end
   end
